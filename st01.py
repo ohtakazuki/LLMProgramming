7f8b06a8e7ba7fd9bcda70f33650a5210b3df4b3
kazuki ohta <ohtakazuki@gmail.com>
2023-07-18 14:19:57 +0900

    Update st01.py

--- st01.py
+++ st01.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-# st.text_input("名前を入力してください", key="name")
-# name = st.session_state.name
-
 name = st.text_input("名前を入力してください")
 
 if name!="":
     f"こんにちは！{name}さん"
-
-"st.session_state.name:" + st.session_state.name
